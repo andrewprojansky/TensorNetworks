Fixes normalization/conjugation issue

TensorNetworks.py
@@ -10,6 +10,9 @@ Created on Sun Oct 30 20:27:10 2022
 import numpy as np
 
 def Psi_1(i_s, dim):
+    '''
+    Initializes first Psi matrix to be 2 x d^(L-1)
+    '''
     
     Psi = np.zeros((2,dim//2))
     for i in range(dim//2):
@@ -18,6 +21,10 @@ def Psi_1(i_s, dim):
     return Psi
 
 def Make_TN(Psi, dim):
+    '''
+    Makes TN for each site, getting A-sigma matrices at each site with left 
+    normalization at all except for last
+    '''
     
     TN = {}
     prank = 1
@@ -29,23 +36,27 @@ def Make_TN(Psi, dim):
         u = u[:, :rank]
         u = u.reshape((2,prank,rank))
         TN[i] = u
-        vh = vh[:,:rank]
-        Psi = np.reshape(np.matmul(np.diag(s), np.transpose(vh)), (rank*2, 2**(dim-2-i)))
+        vh = vh[:rank,:]
+        Psi = np.reshape(np.matmul(np.diag(s), vh), (rank*2, 2**(dim-2-i)))
+        print(Psi)
         prank = rank
     Psi = Psi.reshape(2, 1, rank)
+    #guarentees normalization for final matrix
+    for v in range(2):
+        Psi[v] = Psi[v]/(np.sqrt(np.matmul(Psi[v][0], Psi[v][0])))
     TN[dim-1] = Psi
     return TN
 
 def Multiplication(TN, bi: str):
     if len(TN.keys()) != len(bi):
-        print('projansky youre stinky')
+        print('Need new string')
         return
     else:
         return np.linalg.multi_dot([x[int(bi[i])] for i,x in enumerate(TN.values())])
     
-
 dim = 4
 initial_state = np.array([1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1])
 Psi = Psi_1(initial_state, 2**dim)
 TN = Make_TN(Psi, dim)
 print(TN)
+Multiplication(TN, '1111')